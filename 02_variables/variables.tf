99c31c738c3816e5e7c6d9399571b249b8c266ba
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-12-04 17:37:59 +0100

    Ejemplo de variable

---
+++ 02_variables/variables.tf
@@ -0,0 +1,11 @@
+// Plantilla de máquina virtual
+
+variable "plantilla" {
+  default = "Alpine Linux 3.20"
+}
+
+data "opennebula_templates" "busqueda" {
+  name_regex = var.plantilla
+  sort_on    = "register_date"
+  order      = "ASC" # La más reciente
+}
